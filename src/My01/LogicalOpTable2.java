0e126d46bf1f95b294c983ce06dd0100e32b75de
VadimYanovskij <61162599+VadimYanovskij@users.noreply.github.com>
2020-03-04 22:41:46 +0300

    Таблица истинности логических операций true и false заменены на 1 и 0

---
+++ src/My01/LogicalOpTable2.java
@@ -0,0 +1,54 @@
+package My01;
+
+import javax.swing.*;
+
+public class LogicalOpTable2 {
+    public static void main(String args[]) {
+
+        boolean p, q;
+
+        System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
+
+        p = true; q = true;
+        System.out.print(p + "\t" + q +"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        System.out.print("1" + "\t" + "1" +"\t");
+
+        if ((p&q)==true)
+            System.out.print("1\t" );
+        else
+            System.out.print("0\t");
+
+        if ((p|q)==true)
+            System.out.print("1\t" );
+        else
+            System.out.print("0\t");
+
+        if ((p^q)==true)
+        System.out.print("1\t" );
+        else
+        System.out.print("0\t");
+
+        if ((!p)==true)
+        System.out.println("1\t" );
+        else
+        System.out.println("0\t");
+
+//        p = true; q = false;
+//        System.out.print(p + "\t" + q +"\t");
+//        System.out.print((p&q) + "\t" + (p|q) + "\t");
+//        System.out.println((p^q) + "\t" + (!p));
+//
+//        p = false; q = true;
+//        System.out.print(p + "\t" + q +"\t");
+//        System.out.print((p&q) + "\t" + (p|q) + "\t");
+//        System.out.println((p^q) + "\t" + (!p));
+//
+//        p = false; q = false;
+//        System.out.print(p + "\t" + q +"\t");
+//        System.out.print((p&q) + "\t" + (p|q) + "\t");
+//        System.out.println((p^q) + "\t" + (!p));
+    }
+}
