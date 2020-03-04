e1974c6f987c1cc48b71dec3f661a322e1872dc1
VadimYanovskij <61162599+VadimYanovskij@users.noreply.github.com>
2020-03-04 22:55:40 +0300

    Таблица истинности логических операций true и false заменены на 1 и 0 Полная таблица

--- src/My01/LogicalOpTable2.java
+++ src/My01/LogicalOpTable2.java
@@ -36,19 +36,85 @@ public class LogicalOpTable2 {
         else
         System.out.println("0\t");
 
-//        p = true; q = false;
-//        System.out.print(p + "\t" + q +"\t");
-//        System.out.print((p&q) + "\t" + (p|q) + "\t");
-//        System.out.println((p^q) + "\t" + (!p));
-//
-//        p = false; q = true;
-//        System.out.print(p + "\t" + q +"\t");
-//        System.out.print((p&q) + "\t" + (p|q) + "\t");
-//        System.out.println((p^q) + "\t" + (!p));
-//
-//        p = false; q = false;
-//        System.out.print(p + "\t" + q +"\t");
-//        System.out.print((p&q) + "\t" + (p|q) + "\t");
-//        System.out.println((p^q) + "\t" + (!p));
+        p = true; q = false;
+        System.out.print(p + "\t" + q +"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        System.out.print("1" + "\t" + "0" +"\t");
+
+        if (p&q)
+            System.out.print("1\t" );
+        else
+            System.out.print("0\t");
+
+        if (p|q)
+            System.out.print("1\t" );
+        else
+            System.out.print("0\t");
+
+        if (p^q)
+            System.out.print("1\t" );
+        else
+            System.out.print("0\t");
+
+        if (!p)
+            System.out.println("1\t" );
+        else
+            System.out.println("0\t");
+
+        p = false; q = true;
+        System.out.print(p + "\t" + q +"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        System.out.print("0" + "\t" + "1" +"\t");
+
+        if (p&q)
+            System.out.print("1\t" );
+        else
+            System.out.print("0\t");
+
+        if (p|q)
+            System.out.print("1\t" );
+        else
+            System.out.print("0\t");
+
+        if (p^q)
+            System.out.print("1\t" );
+        else
+            System.out.print("0\t");
+
+        if (!p)
+            System.out.println("1\t" );
+        else
+            System.out.println("0\t");
+
+        p = false; q = false;
+        System.out.print(p + "\t" + q +"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        System.out.print("0" + "\t" + "0" +"\t");
+
+        if (p&q)
+            System.out.print("1\t" );
+        else
+            System.out.print("0\t");
+
+        if (p|q)
+            System.out.print("1\t" );
+        else
+            System.out.print("0\t");
+
+        if (p^q)
+            System.out.print("1\t" );
+        else
+            System.out.print("0\t");
+
+        if (!p)
+            System.out.println("1\t" );
+        else
+            System.out.println("0\t");
     }
 }
