18612516dd0ebaea052b56f5f5e7f6d3d51fd4ca
VadimYanovskij <myemail11@gmail.com>
2020-03-04 13:56:27 +0300

    Дюймы в метры

---
+++ src/My01/GalToLitTable.java
@@ -0,0 +1,22 @@
+package My01;
+
+public class GalToLitTable {
+    public static void main(String[] args) {
+        double inchs, metres;
+        int counter;
+
+        counter = 0;
+        for(inchs = 2; inchs <= 144; inchs=inchs+2) {
+            metres = inchs / 39.37; // convert to liters
+            System.out.println(inchs + " inchs is " +
+                    metres + " metres.");
+
+            counter++;
+            // every 10th line, print a blank line
+            if(counter == 6) {
+                System.out.println();
+                counter = 0; // reset the line counter
+            }
+        }
+    }
+}
