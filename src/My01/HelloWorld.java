690485743d80e4a3e2b7a0f86b22c10ad597c253
VadimYanovskij <myemail11@gmail.com>
2020-03-04 13:53:54 +0300

    Привет, мир! Дюймы в метры

---
+++ src/My01/HelloWorld.java
@@ -0,0 +1,14 @@
+package My01;
+
+public class HelloWorld {
+//    // A Java program begins with a call to main().
+    public static void main(String[] args) {
+        System.out.println("Java drives the Web.");
+    }
+}
+
+//public class HelloWorld {
+//    public static void main(String args[]) {
+//        System.out.println("Hello, World!");
+//    }
+//}
