99afe3bb7b34ce04bdfac03fd1c5b5b0df00b46c
VadimYanovskij <61162599+VadimYanovskij@users.noreply.github.com>
2020-03-05 21:16:15 +0300

    Таблица простых чисел от 1 до 100. Ответ из учебника((

--- src/My01/PrimeNumbers.java
+++ src/My01/PrimeNumbers.java
@@ -2,20 +2,18 @@ package My01;
 
 public class PrimeNumbers {
     public static void main(String[] args) {
-        int i, j;
+      int i,j;
 
 //        System.out.print("2\t3\t5\t7\t11\t");
 //        for (i=8; i<=100; i++)
 //            if (((i % 2)!=0)&&((i % 3)!=0)&&((i % 5)!=0)&&((i % 7)!=0)&&((i % 11)!=0)) System.out.print(i + "\t");
 
 
-        boolean isprime;
+      boolean isprime;
         for (i = 2; i <= 100; i++) {
             isprime = true;
-            for (j = 2; j <= i / j; j++) {
+            for (j = 2; j <= i/j; j++)
                 if ((i % j) == 0) isprime = false;
-                System.out.println("i=" + i + "\tj=" + j);
-            }
             if (isprime)
                 System.out.println(i + " простое");
 
@@ -23,3 +21,4 @@ public class PrimeNumbers {
         }
     }
 }
+
